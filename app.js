8e91e88b16e7a089c46db1b20ebac42f06a7211c
Jordan Wong <pjwong97@gmail.com>
2022-07-19 16:23:26 -0700

    finish adfGif function and found gin data structure

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
+'use strict';
 console.log("Let's get this party started!");
 
-// When the user submits the form, use axios to make a request to GIPHY 
-// for information based on that term. 
+// When the user submits the form, use axios to make a request to GIPHY
+// for information based on that term.
 
 
 // function to pull data
 
-async function getGif() {
+async function getGif(event) {
+
+    event.preventDefault();
+
     let searchTerm = $("#inputField").val();
     //diffence between val and text here?
 
@@ -14,15 +18,25 @@ async function getGif() {
     let response = await axios.get(
         `http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`)
 
-// After receiving the response, console.log the response data to make 
+// After receiving the response, console.log the response data to make
 // sure you’re getting back what you expect.
     console.log("got", response);
+    console.log(response.data.data[0].images.original.url);
     return response.data;
+
+}
+
+function addGif(gifUrl){
+
+    $('#gifList').append($(`<div class = "new-gif"><img src = "${gifUrl}"></div>`));
 
 }
 
 // event handler for the button
 
+$('#searchButton').on('click', getGif);
+
+//add remove gifs button
 
 
 
